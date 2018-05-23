3e35265618bb24a42dc65dcc749533b6645f2f48
paulohenriquefreitas <paulohfreitas_ti@hotmail.com>
2018-05-23 09:51:43 -0300

    Improving

--- keepAlive.sh
+++ keepAlive.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-while true; do
-    sleep 2
-    path=$1
-    file2=$2
+echo "starting tunnels .............................................................."
 
-    for host in $(cat $HOME/$path/$file2)
+if [ ! -z $1 ]
+then
+    sleep=$1
+else
+    sleep=5
+fi    
+echo $OSTYPE
+
+echo $PWD
+echo $sleep
+
+for host in $(cat $PWD/tunnelList.txt); do
+    echo $host
+	(sshpass -p "JkliuPh3" ssh -N -p 22 tr568073@mbchx03b -L $host) &
+done
+
+while true; do
+    sleep $sleep    
+
+    for host in $(cat $PWD/tunnelList.txt)
     do
     	if [ ! -z $host ]; then	
             r=$host	
             ntstat=`sudo netstat -lnp | grep ssh | grep :${r%%:*}`    
             if [ -z "$ntstat" ]
             then
-                sshpass -p "JkliuPh3" ssh -N -p 22 tr568073@mbchx03b -L $host
+                (sshpass -p "JkliuPh3" ssh -N -p 22 tr568073@mbchx03b -L $host) &
                 echo "Reconnecting " $host
             else
                 echo $ntstat            
             fi    
-    	fi
+    	fi        
     done
+    echo "Check connection after "  $sleep " seconds"
 done
